b6a5d33cf89f7fcf642f8c1b563a551e85ff3cdb
ONESECTOR <onesector.dev@gmail.com>
2025-11-30 20:59:01 +0300

    Изменение времени отправки на 13:00

--- config.py
+++ config.py
@@ -7,7 +7,7 @@ BOSS_USERNAME = os.getenv('BOSS_USERNAME', 'gr8brite')
 BOSS_CHAT_ID = os.getenv('BOSS_CHAT_ID')  # ID чата с боссом для отправки вопросов
 
 START_DATE = datetime(2025, 11, 11)
-SEND_TIME = "12:00"
+SEND_TIME = "13:00"
 
 MESSAGE_TEMPLATE = "Разрабатываю игру день {}"
 VACATION_HASHTAG = "\n#выходной@greatbritedevelop"
